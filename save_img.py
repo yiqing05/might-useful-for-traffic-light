526a6ee2d6c46ec9bc42344119ba48f1247701bb
yiqing05 <49597979+yiqing05@users.noreply.github.com>
2019-09-21 15:38:43 -0700

    Update save_img.py

--- save_img.py
+++ save_img.py
@@ -12,6 +12,14 @@ import matplotlib.pyplot as plt
 #cap = cv2.VideoCapture("F:\\traffic\\sun\\q1.avi") #读入视频文件
 
 def save_img(cap, save_path, count,start,bgr=True):   
+    '''
+    for save img from existed rain/sun avi bgr video
+    cap: video loaded by Opencv
+    save_path:folder for saving img
+    count:save a image between the number of frames
+    start:save image name
+    output: images saved in 'save_path' folder
+    '''
 #    flag = 0
     c=1
     while (cap.isOpened()):
@@ -45,6 +53,11 @@ def save_img_time(cap,save_path,lenth,rgb=False):
     cap.release()
 
 def find_boundary(img):
+    '''
+    for self recorded images, to find the IOU boundaries.
+    inout: RGB images
+    output: array with (xmin, ymin, height, width)
+    '''
 #    img = cv2.imread(img_path)
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     binaryImg = cv2.Canny(gray,100,250)
@@ -72,8 +85,12 @@ def find_boundary(img):
 
 def find_bbox(img, rgb = True):
     '''
+    just to recogenize the annotation box after object detection 
+    might need to change the color array when the result box color is different than mine
+    the color array should be in hsv coordinate
     box (0,255,255) yellow
-    color_array shold be in hsv coordinate
+    input:image after detection
+    output:smallest bounding box which include all detected object
     '''
     if rgb:
         hsv_img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
@@ -84,8 +101,8 @@ def find_bbox(img, rgb = True):
 #    upper_color = color_array + np.array([10,10,10])
 #  array([ 55, 152, 151], dtype=uint8)
 #        array([ 54, 112, 116], dtype=uint8)
-    lower_color =  np.array([40,110,110])
-    upper_color = np.array([64,160,160])
+    lower_color =  np.array([40,110,110])#might need change here
+    upper_color = np.array([64,160,160])#might need change here
     mask = cv2.inRange(hsv_img, lower_color, upper_color)
 #    cv2.imshow('mask',mask)
     h = cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
@@ -117,7 +134,14 @@ def find_bbox(img, rgb = True):
 #        return np.array([])
     return np.array([xmin,ymin,wmax,hmax])
 
-def draw_ROI(coor,img,color,label1,label2,path):
+def draw_ROI(coor,img,color,path):
+    '''
+    draw ROI area in public based images, this function CANNOT segment images, just test
+    coor:location array from find_bbox 
+    img:origional image
+    color:ROI area color
+    path: new image save path
+    '''
     x,y,w,h = coor
     a,b = np.shape(img[:,:,0])
     if w >= a or h >= b:
@@ -132,11 +156,12 @@ def draw_ROI(coor,img,color,label1,label2,path):
         y = b-h-1
     cv2.rectangle(img,(x,y),(x+w,y+h),color,1)
     font = cv2.FONT_HERSHEY_PLAIN 
-    cv2.putText(img,label1,(x,y+h+17), font, 1, color,1)
-    cv2.putText(img,label2,(x,y+h+34), font, 1, color,1)
+    #cv2.putText(img,label1,(x,y+h+17), font, 1, color,1)
+    #cv2.putText(img,label2,(x,y+h+34), font, 1, color,1)
     cv2.imwrite(path, img)
 
 def find_ratio(path):
+    
     print('finding ratio...')
     tiny_rec = []
     large_rec = [] 
